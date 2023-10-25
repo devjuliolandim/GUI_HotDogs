242a98b6fc7d0d50b527373c0eb2f36e48a3551a
Júlio César Landim <juliocesarlandim@alu.ufc.br>
2023-10-25 16:18:13 -0300

    first commit]

---
+++ laboratorios/src/aprasentacao/Label.java
@@ -0,0 +1,12 @@
+package aprasentacao;
+
+public class Label {
+
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ laboratorios/src/lab1/Main.java
@@ -0,0 +1,13 @@
+package lab1;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		new Barraquinha();
+		
+		
+	}
+
+}
